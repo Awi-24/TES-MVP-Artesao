ef3df9ca65b1d38d4273b5ef3330d7a691d34521
RodrigoMacedo99 <104858896+RodrigoMacedo99@users.noreply.github.com>
2025-06-27 01:52:34 -0300

    feat: Configura Jest e adiciona testes unitários

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,6 @@
-const { createDefaultPreset } = require("ts-jest");
-
-const tsJestTransformCfg = createDefaultPreset().transform;
-
-/** @type {import("jest").Config} **/
+/** @type {import('jest').Config} */
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  
-  // GARANTA QUE ESTA LINHA ESTEJA ASSIM:
-  testMatch: ['**/__tests__/**/*.test.ts'],
-
-  testPathIgnorePatterns: ['/node_modules/', '/dist/'],
+  roots: ['<rootDir>/backend'],
 };
